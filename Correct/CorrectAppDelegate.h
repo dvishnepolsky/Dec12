b18c0a942dab1a721bcfd0b3bfc441416286d0b3
nyuguest <nyuuser@ITS8304WK-MX.(none)>
2013-04-13 16:02:25 -0400

    Initial Commit

---
+++ Correct/CorrectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CorrectAppDelegate.h
+//  Correct
+//
+//  Created by nyuguest on 4/13/13.
+//  Copyright (c) 2013 nyuguest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CorrectAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Correct/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Correct
+//
+//  Created by nyuguest on 4/13/13.
+//  Copyright (c) 2013 nyuguest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CorrectAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([CorrectAppDelegate class]));
+	}
+}
